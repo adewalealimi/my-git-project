00b9f97a1380218b170d72e3cf4ffe919971a160
adewalealimi <alimiadewale68@gmail.com>
2021-01-07 04:51:02 -0800

    Added gitiignore file and deleted git cache

--- my_file.py
+++ my_file.py
@@ -2,10 +2,3 @@ def sum(a, b):
     return a + b
 
 print(sum(4, 5))
-
-# My Git Project
-
-
-> My code for the git project
-
-*Fork from [es6-webpack2-starter](https://github.com/micooz/es6-webpack2-starter)*
